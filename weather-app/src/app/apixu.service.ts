c5749b3c3ac36d4888d0f23c9e53612ae9f912b6
profiledror <dror@profilesoft.com>
2021-06-17 02:09:51 +0300

    Added weahter service

---
+++ weather-app/src/app/apixu.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+const apiKey: string = environment.apiKey;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApixuService {
+
+  constructor(private http: HttpClient) {}
+  getWeather(location: any, units: any) {
+    return this.http.get(`${environment.apiUrl}/weather?q=${location}&appid=${apiKey}&mode=json&units=${units}`)
+    
+  }
+}
