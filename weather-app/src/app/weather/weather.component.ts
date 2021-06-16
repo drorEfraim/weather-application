f14ab854af268bbb0d66895da27b691f609c7d54
profiledror <dror@profilesoft.com>
2021-06-17 02:11:33 +0300

    Added weather basic logic

--- weather-app/src/app/weather/weather.component.ts
+++ weather-app/src/app/weather/weather.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { ApixuService } from "../apixu.service";
 
 @Component({
-  selector: 'app-weather',
-  templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.css']
+  selector: "app-weather",
+  templateUrl: "./weather.component.html",
+  styleUrls: ["./weather.component.css"]
 })
 export class WeatherComponent implements OnInit {
+  public weatherSearchForm: FormGroup;
+  public weatherData: any;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private apixuService: ApixuService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.weatherSearchForm = this.formBuilder.group({
+      location: [""],
+      units: [""],
+    });
   }
 
+  sendToAPIXU(formValues) {
+    this.apixuService.getWeather(formValues.location, formValues.units).subscribe(data => {
+      this.weatherData = data;
+      console.log(this.weatherData);
+    });
+  }
 }
